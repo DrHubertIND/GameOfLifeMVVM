62ee8ce233c41bc4e913d6ce5b1d8d838fda119b
DrHubertIND <evgeny.e@ind.studio>
2024-03-22 18:11:20 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using GameOfLifeMVVM.Model;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfLifeMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int _rows;
+        private int _columns;
+        private GOFModel? _model;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawFieldClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtRows.Text, out int rows) && int.TryParse(txtColumns.Text, out int columns))
+            {
+                _rows = rows;
+                _columns = columns;
+
+                NewGrid(_rows, _columns);
+
+                _model = new(_rows, _columns);
+
+                Draw(_model.Grid);
+            }
+            else
+            {
+                MessageBox.Show("Invalid input! Please enter valid row and column counts.");
+            }
+        }
+
+        private void NewGrid(in int rows, in int columns)
+        {
+            
+            ClearGrid();
+            for (int i = 0; i < rows; i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                gridGame.RowDefinitions.Add(rowDefinition);
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                gridGame.ColumnDefinitions.Add(columnDefinition);
+            }
+        }
+
+        private void ClearGrid()
+        {
+            gridGame.Children.Clear();
+            gridGame.RowDefinitions.Clear();
+            gridGame.ColumnDefinitions.Clear();
+        }
+        private void Draw()
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    Border border = new Border();
+                    border.BorderBrush = Brushes.RoyalBlue;
+                    border.BorderThickness = new Thickness(1);
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+                    gridGame.Children.Add(border);
+                }
+            }
+        }
+
+        private void Draw(in int[,] gof)
+        {
+            NewGrid(_rows, _columns);
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.Stroke = Brushes.RoyalBlue;
+                    rectangle.StrokeThickness = 1;
+                    rectangle.Fill = gof[i, j] == 1 ? Brushes.RoyalBlue : Brushes.Transparent;
+                    Grid.SetRow(rectangle, i);
+                    Grid.SetColumn(rectangle, j);
+                    gridGame.Children.Add(rectangle);
+                }
+            }
+        }
+
+        async private void GameClick(object sender, EventArgs e) 
+        {
+            ClearGrid();
+            Draw(_model.Grid);
+        }
+
+        private void StopGameClick(object sender, EventArgs e)
+        {
+            ClearGrid();
+        }
+
+    }
+}
---
+++ Model/GOFModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GameOfLifeMVVM.Model
+{
+    class GOFModel
+    {
+        private int[,] grid_;
+
+        public GOFModel(int rows, int columns) 
+        {
+            grid_ = new int[rows,columns];
+            RandomInit();
+        }
+
+        private void RandomInit() {
+            var rnd = new Random();
+            for (int i = 0; i < grid_.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid_.GetLength(1); j++)
+                {
+                    grid_[i, j] = rnd.Next(0, 2);
+                }
+            }
+        }
+
+        private int[,] NextGeneration()
+        {
+            int width = grid_.GetLength(0);
+            int height = grid_.GetLength(1);
+
+            int[,] nextGeneration = new int[width,height];
+
+            for (int i = 0;i < width;i++)
+            {
+                for (int j = 0;j < height;j++)
+                {
+                    int liveNeighbors = CountLiveNeighbors(i, j);
+                    if (grid_[i, j] == 1)
+                    {
+                        if (liveNeighbors == 2 || liveNeighbors == 3)
+                        {
+                            nextGeneration[i, j] = 1;
+                        }
+                        else
+                        {
+                            nextGeneration[i, j] = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (liveNeighbors == 3)
+                        {
+                            nextGeneration[i, j] = 1;
+                        }
+                        else
+                        {
+                            nextGeneration[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            return nextGeneration;
+        }
+
+        private int CountLiveNeighbors(int x, int y)
+        {
+            int result = 0;
+
+            int wigth = grid_.GetLength (0);
+            int height = grid_ .GetLength (1);
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int neighborX = (x + i + wigth) % wigth;
+                    int neighborY = (y + j + height) % height;
+
+                    if (!(i == 0 && j == 0))
+                    {
+                        result += grid_[neighborX, neighborY];
+                    }
+                }
+            }
+
+            return result;
+        }
+        public int[,] Grid { get 
+            {
+                grid_ = NextGeneration();
+                return grid_;
+            } }
+    }
+}
